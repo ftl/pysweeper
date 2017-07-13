e5662e9dd028d2718675ee624fefe0aee9e37c22
Florian Thienel <ftl@thecodingflow.com>
2017-07-13 20:26:29 +0200

    extract sweeping parameters into own widget

--- gui.py
+++ gui.py
@@ -32,21 +32,7 @@ class MainWindow(QMainWindow):
         self.connect_button.clicked.connect(self.connect_sweeper)
         self.disconnect_button = QPushButton('&Disconnect', sidebar)
         self.disconnect_button.clicked.connect(self.disconnect_sweeper)
-
-        start_frequency_label = QLabel('Start Frequency [Hz]', sidebar)
-        self.start_frequency_edit = QSpinBox(sidebar)
-        self.start_frequency_edit.setRange(1000000, 30000000)
-        self.start_frequency_edit.setValue(13500000)
-        stop_frequency_label = QLabel('Stop Frequency [Hz]', sidebar)
-        self.stop_frequency_edit = QSpinBox(sidebar)
-        self.stop_frequency_edit.setRange(1000000, 30000000)
-        self.stop_frequency_edit.setValue(14500000)
-        steps_label = QLabel('Steps', sidebar)
-        self.steps_edit = QSpinBox(sidebar)
-        self.steps_edit.setRange(10, 9999)
-        self.steps_edit.setValue(100)
-        self.sweep_button = QPushButton('&Start Sweep', sidebar)
-        self.sweep_button.clicked.connect(self.sweep)
+        self.sweep_tab = SweepTab(sidebar)
 
         self.plotter = SweepPlotter(app, root_frame)
 
@@ -55,13 +41,7 @@ class MainWindow(QMainWindow):
         sidebar_layout.addWidget(self.port_edit)
         sidebar_layout.addWidget(self.connect_button)
         sidebar_layout.addWidget(self.disconnect_button)
-        sidebar_layout.addWidget(start_frequency_label)
-        sidebar_layout.addWidget(self.start_frequency_edit)
-        sidebar_layout.addWidget(stop_frequency_label)
-        sidebar_layout.addWidget(self.stop_frequency_edit)
-        sidebar_layout.addWidget(steps_label)
-        sidebar_layout.addWidget(self.steps_edit)
-        sidebar_layout.addWidget(self.sweep_button)
+        sidebar_layout.addWidget(self.sweep_tab)
         sidebar_layout.addStretch(100)
         sidebar.setLayout(sidebar_layout)
  
@@ -69,6 +49,9 @@ class MainWindow(QMainWindow):
         root_layout.addWidget(sidebar, 20)
         root_layout.addWidget(self.plotter, 80)
         root_frame.setLayout(root_layout)
+
+        self.sweep_tab.start_sweep.connect(self.plotter.sweep)
+        self.sweep_tab.start_sweep.connect(self.sweeper.sweep)
 
         self.sweeper.connection_opened.connect(self.sweeper_connected)
         self.sweeper.connection_closed.connect(self.sweeper_disconnected)
@@ -83,14 +66,6 @@ class MainWindow(QMainWindow):
     def disconnect_sweeper(self):
         self.sweeper.close_connection()
 
-    def sweep(self):
-        start_frequency = self.start_frequency_edit.value()
-        stop_frequency = self.stop_frequency_edit.value()
-        steps = self.steps_edit.value()
-
-        self.plotter.sweep(start_frequency, stop_frequency)
-        self.sweeper.sweep(start_frequency, stop_frequency, steps)
-
     def sweeper_connected(self):
         self._update_enablement(True)
 
@@ -100,15 +75,55 @@ class MainWindow(QMainWindow):
     def _update_enablement(self, sweeper_connected):
     	self.connect_button.setEnabled(not(sweeper_connected))
     	self.disconnect_button.setEnabled(sweeper_connected)
-    	self.sweep_button.setEnabled(sweeper_connected)
+    	self.sweep_tab.setEnabled(sweeper_connected)
+
+class SweepTab(QWidget):
+    start_sweep = Signal(int, int, int)
+
+    def __init__(self, parent = None):
+        QWidget.__init__(self, parent)
+
+        start_frequency_label = QLabel('Start Frequency [Hz]', self)
+        self.start_frequency_edit = QSpinBox(self)
+        self.start_frequency_edit.setRange(1000000, 30000000)
+        self.start_frequency_edit.setValue(13500000)
+        stop_frequency_label = QLabel('Stop Frequency [Hz]', self)
+        self.stop_frequency_edit = QSpinBox(self)
+        self.stop_frequency_edit.setRange(1000000, 30000000)
+        self.stop_frequency_edit.setValue(14500000)
+        steps_label = QLabel('Steps', self)
+        self.steps_edit = QSpinBox(self)
+        self.steps_edit.setRange(10, 9999)
+        self.steps_edit.setValue(100)
+        sweep_button = QPushButton('&Start Sweep', self)
+        sweep_button.clicked.connect(self._sweep)
+
+        layout = QVBoxLayout()
+        layout.addWidget(start_frequency_label)
+        layout.addWidget(self.start_frequency_edit)
+        layout.addWidget(stop_frequency_label)
+        layout.addWidget(self.stop_frequency_edit)
+        layout.addWidget(steps_label)
+        layout.addWidget(self.steps_edit)
+        layout.addWidget(sweep_button)
+        self.setLayout(layout)
+
+    def _sweep(self):
+        start_frequency = self.start_frequency_edit.value()
+        stop_frequency = self.stop_frequency_edit.value()
+        steps = self.steps_edit.value()
+
+        self.start_sweep.emit(start_frequency, stop_frequency, steps)
 
 class SweepPlotter(pg.PlotWidget):
     def __init__(self, app, parent):
         pg.PlotWidget.__init__(self, parent)
-        self.disableAutoRange(pg.ViewBox.XAxis)
         self.getAxis('bottom').setLabel('f [Hz]')
         self.getAxis('left').setLabel('VSWR')
         self.sweep_plot = self.plot()
+        self.max_vswr = 0
+        self.min_vswr = sys.maxsize
+        self.min_vswr_frequency = 0
         self.min_line = self.addLine(x=0)
         self.qt_app = app
         self._reset_data()
@@ -116,10 +131,15 @@ class SweepPlotter(pg.PlotWidget):
     def sweep(self, start_frequency, stop_frequency):
         self._reset_data()
         self.setXRange(start_frequency, stop_frequency)
+        self.setYRange(0.9, 2.0)
 
     def add_data_point(self, frequency, vswr, forward, reverse):
         self.x_values.append(frequency)
         self.y_values.append(vswr)
+        if (self.max_vswr < vswr):
+            self.max_vswr = vswr
+        if (self.max_vswr > self.viewRange()[1][1]):
+            self.setYRange(0.9, self.max_vswr)
         if (self.min_vswr > vswr):
             self.min_vswr = vswr
             self.min_vswr_frequency = frequency
@@ -130,5 +150,6 @@ class SweepPlotter(pg.PlotWidget):
     def _reset_data(self):
         self.x_values = []
         self.y_values = []
+        self.max_vswr = 0
         self.min_vswr = sys.maxsize
         self.min_vswr_frequency = 0
